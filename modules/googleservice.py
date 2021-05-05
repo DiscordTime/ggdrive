DiscordTime/ggdrive

commit e4101a3f8197acc6f7ded2d02043b808882850e3

In 'list' feature, remove printed lines before printing new ones.
Fixes #52

modules/googleservice.py
@@ -93,20 +93,24 @@ class GoogleService:
         if filename is not None:
             # Could also be name = '%s' for an exact search
             query = "name contains '%s'" % filename
-        fields = ('nextPageToken, files/id, files/name, files/size, '
-                  'files/modifiedTime, files/modifiedByMeTime, files/owners')
-        search = self.drive().list(
-            q=query,
-            orderBy='modifiedByMeTime desc',
-            fields=fields,
-            pageSize=page_size,
-            pageToken=page_token
-        ).execute(http=self.create_http())
-        files_found = search.get('files', [])
-        if files_found and page_size == 1:
-            return files_found[0], None
-        next_page = search.get('nextPageToken', None)
-        return files_found, next_page
+        fields = ','.join(
+            ('nextPageToken',) +
+            tuple(map(lambda x: 'files/' + x, ('id', 'name', 'size', 'modifiedTime', 'modifiedByMeTime', 'owners')))
+        )
+        http = self.create_http()
+        while True:
+            search = self.drive().list(
+                q=query,
+                orderBy='modifiedByMeTime desc',
+                fields=fields,
+                pageSize=page_size,
+                pageToken=page_token
+            ).execute(http=http)
+            files_found = search.get('files', [])
+            page_token = search.get('nextPageToken', None)
+            yield files_found
+            if page_token is None:
+                break
 
     def get_last_modified_file(self):
         file, _ = self.search_filename(None)

modules/util.py
@@ -1,6 +1,7 @@
 import mimetypes
 import os
 import sys
+from typing import Callable, Any
 
 from modules import logger
 
@@ -41,22 +42,33 @@ def guess_mimetype(filepath):
     return guessed_type
 
 
-def describe_files(*metadatas):
-    """Print files metadata"""
+def files_descriptions(*metadata):
+    """Returns formatted string representations of files metadata"""
 
-    def describe():
-        print("Name: %s" % metadata["name"])
-        print("Owner: %s" % metadata["owners"][0]["displayName"])
-        if 'size' in metadata:
-            print("Size: %s" % to_human_readable(float(metadata["size"])))
-        if 'modifiedByMeTime' in metadata:
-            print("Modified by Me: %s" % metadata["modifiedByMeTime"])
-        else:
-            print("Modified: %s" % metadata["modifiedTime"])
-        print("ID: %s\n" % metadata["id"])
+    def description(m):
+        by_me = 'modifiedByMeTime' in m
+        modified_by = m['modifiedByMeTime' if by_me else 'modifiedTime']
+        desc = [
+            f"Name: {m['name']}",
+            f"Owner: {m['owners'][0]['displayName']}",
+            "Size: {0}".format(*to_human_readable(float(m['size']))) if 'size' in m else None,
+            "Modified{0}: {1}".format(" by me" if by_me else "", modified_by),
+            f"ID: {m['id']}"
+        ]
+        return [d for d in desc if d is not None]
 
-    for metadata in metadatas:
-        describe()
+    return [description(m) for m in metadata]
+
+
+def print_files_descriptions(*descriptions, line_sep='\n', desc_sep='\n'):
+    for desc in descriptions:
+        for line in desc:
+            print(line, end=line_sep)
+        print(end=desc_sep)
+
+
+def describe_files(*metadata, line_sep='\n', desc_sep='\n'):
+    print_files_descriptions(files_descriptions(*metadata), line_sep, desc_sep)
 
 
 def current_python_version():
@@ -127,3 +139,18 @@ def copy_file_contents(dest: str, *srcs: str):
             with open(src, 'rb') as partial_file:
                 logger.d(f"Writing '{partial_file.name}' into '{dest}'")
                 final_file.write(partial_file.read())
+
+
+def delete_lines(lines: int = 1):
+    for _ in range(lines):
+        print('\x1b[2K', end='\n')
+
+
+def move_cursor_up(lines: int = 1):
+    for _ in range(lines):
+        print('\x1b[1A', end='\r')
+
+
+def for_lines(lines: int = 1, *funcs: Callable[[int], Any]):
+    for func in funcs:
+        func(lines)

modules/command.py
@@ -4,7 +4,8 @@ from modules import extractor, logger, config
 from modules.chunks import Chunks
 from modules.googleservice import GoogleService
 from modules.progresslogger import ProgressLogger, Progress
-from modules.util import describe_files, current_is_python36, find_last_modified_file, guess_mimetype
+from modules.util import current_is_python36, find_last_modified_file, guess_mimetype, move_cursor_up, \
+    delete_lines, for_lines, files_descriptions, print_files_descriptions, describe_files
 
 
 class Command:
@@ -277,25 +278,17 @@ class List(Command):
         self.list_files()
 
     def list_files(self):
-        files_found, next_page = self.search()
-        describe_files(*files_found)
-        while next_page is not None:
-            try:
-                if input(">>> Press Enter for the next results\n"):
-                    break
-                files_found, next_page = self.search(next_page)
-                describe_files(*files_found)
-            except KeyboardInterrupt:
-                return
+        num_printed_lines = 0
+        for files_found in self.search():
+            for_lines(num_printed_lines, move_cursor_up, delete_lines, move_cursor_up)
+            descs = files_descriptions(*files_found)
+            print_files_descriptions(*descs)
+            if input(">>> Press Enter for the next results\n"):
+                break
+            num_printed_lines = len(descs) + sum(map(len, descs)) + 2  # last 2 lines are input text + newline
 
-    def search(self, next_page=None):
-        search_query = self.args.file
-        files_found, new_next_page = self.google.search_filename(
-            search_query,
-            self.FILES_PER_PAGE,
-            next_page
-        )
-        return files_found, new_next_page
+    def search(self):
+        yield from self.google.search_filename(self.args.file, self.FILES_PER_PAGE)
 
 
 class CommandParser:

commit 30cd4d193ef53591204f8bb76c8f474905926ee8

Fix --last option
--last option was broken by e4101a3f8197acc6f7ded2d02043b808882850e3

Fixes #54

modules/googleservice.py
@@ -113,8 +113,10 @@ class GoogleService:
                 break
 
     def get_last_modified_file(self):
-        file, _ = self.search_filename(None)
-        return file
+        try:
+            return next(self.search_filename())[0]
+        except KeyError:
+            return None
 
     def get_file_downloader(self, metadata):
         def file_downloader(start: int, end: int) -> bytes:

modules/util.py
@@ -68,7 +68,7 @@ def print_files_descriptions(*descriptions, line_sep='\n', desc_sep='\n'):
 
 
 def describe_files(*metadata, line_sep='\n', desc_sep='\n'):
-    print_files_descriptions(files_descriptions(*metadata), line_sep, desc_sep)
+    print_files_descriptions(*files_descriptions(*metadata), line_sep=line_sep, desc_sep=desc_sep)
 
 
 def current_python_version():