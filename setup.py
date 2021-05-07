8e5e34ec92ccaccbc9a462aa140164f074d729d1
Patrick Steiger <psteiger@gmail.com>
2021-05-07 12:26:59 -0300

    Added PyPI distribution package
    - Package distributed by the name 'ggdrive'.
    - Updated README.md.

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ggdrive',
+    version='0.1.2',
+    scripts=['gdrive'],
+    author="DiscordTime",
+    description="A Docker and AWS utility package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DiscordTime/ggdrive",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+)
